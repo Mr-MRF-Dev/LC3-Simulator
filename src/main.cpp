1525e3d8529b996a7864a309b750dbae62c133b5
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-07-07 12:57:23 +0330

    remove mainWindow :office:

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,12 @@
 #include <QApplication>
 
-#include "assembler.h"
 #include "assembler_window.h"
-#include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     AssemblerWindow s;
     s.show();
-
-    Assembler A;
-    s.setWindowTitle(QString::fromStdString(A.getMsg()));
 
     return a.exec();
 }
